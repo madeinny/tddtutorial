9350888f6c3def38639eba495f658ce568568680
a <a@a.com>
2016-12-27 18:11:49 -0500

    Added post support to the to-do 'add' form

--- lists/tests.py
+++ lists/tests.py
@@ -8,7 +8,7 @@ from lists.views import home_page
 
 class HomePageViewTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_uses_home_template(self):
         request = HttpRequest()
         response = home_page(request)
         self.assertIn('<title>To-Do lists</title>', response.content.decode('utf8'))
@@ -16,6 +16,22 @@ class HomePageViewTest(TestCase):
         self.assertTrue(response.content.decode('utf8').strip().endswith('</html>'))
         
         expected_content = render_to_string('home.html')
+        self.assertEqual(
+            response.content.decode('utf8'),
+            expected_content
+        )
+
+    def test_home_page_can_store_post_requests(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'new item'
+        response = home_page(request)
+
+        expected_content = render_to_string(
+            'home.html',
+            {'new_item_text': 'new item'}
+        )
+
         self.assertEqual(
             response.content.decode('utf8'),
             expected_content
