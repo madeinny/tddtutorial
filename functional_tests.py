460232f09792562ee4531dc729f5f423669ec777
a <a@a.com>
2016-12-27 16:50:03 -0500

    Extended functional test to actually input stuff

--- functional_tests.py
+++ functional_tests.py
@@ -16,13 +16,30 @@ class HomePageTest(unittest.TestCase):
         
         # She notices the page title and header metion to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header.text)
 
         # She is invited to enter a to-do item straight away
         # She types "Buy peacock feathers" into a text box
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('\n')
+
         # When she hits enter, the page updates, and now the page lists
         # "1: buy peacock feathers" as an item in a to-do list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         # There is still a text box inviting her to add another item
 
+        self.fail('Finish the test!')
+
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(warnings='ignore')
